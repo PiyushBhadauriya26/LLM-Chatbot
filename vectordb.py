0098f802a85ea5cbf0b9c3816b575f339c9d1b47
Piyush <pibhadau@cisco.com>
2024-05-29 02:13:01 -0700

    update namespace to use combined_disease data

--- vectordb.py
+++ vectordb.py
@@ -5,7 +5,7 @@ load_dotenv()
 from openai import OpenAI
 client = OpenAI()
 class Pinecode_DB:
-    def __init__(self, api_key=None, index_name="medical-kb",name_space="medical"):
+    def __init__(self, api_key=None, index_name="medical-kb",name_space="combined_disease_prediction_symptom (2)"):
         if api_key is None:
             api_key = os.getenv("PINECONE_API_KEY")
         self.pinecone = Pinecone(api_key=api_key)
@@ -13,20 +13,16 @@ class Pinecode_DB:
         self.name_space = name_space
         self.retrieve_limit = 3600  # set the limit of knowledge base words, leave some space for chat history and query.
 
-    def retrieve(self, query, name_space="medical"):
+    def retrieve(self, query, name_space="combined_disease_prediction_symptom (2)"):
         embed_model = "text-embedding-3-small"
         delimiter = "###"
         res = client.embeddings.create(
             input=[query],
             model=embed_model
         )
-        # print("res:", res)
-
         # retrieve from Pinecone knowledge base
         xq = res.data[0].embedding
-        # print("xq:", xq)
         index = self.pinecone.Index(self.index_name)
-
         # get relevant contexts
         res = index.query(vector=xq,
                           top_k=5,
@@ -35,9 +31,6 @@ class Pinecode_DB:
         contexts = [
             x["metadata"]["text"] for x in res["matches"]
         ]
-        # print("contexts:", contexts)
-
-        # build our prompt with the retrieved contexts included
         prompt = " "
         count = 0
         proceed = True
@@ -48,7 +41,6 @@ class Pinecode_DB:
                 prompt += contexts[count]
 
             count += 1
-        # End of while loop
 
         prompt = delimiter + prompt + delimiter
         return prompt
